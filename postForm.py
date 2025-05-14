494d6253de1c1b9e6db3af2cd31cfb69d0aa37ad
Kabir Nahata <kbrnahata@gmail.com>
2025-05-14 15:10:20 +0545

    update data

--- postForm.py
+++ postForm.py
@@ -26,41 +26,23 @@ def next_page():
     name = forms.find("form p:first input").attr('name')
     tel = forms("form p").eq(1).find("input").attr("name")
     email = forms("form p").eq(2).find("input").attr("name")
-    pizza_size_small = forms("form fieldset p label").find("input").attr("value")
-    pizza_size_medium = forms("form fieldset p").eq(1).find("label input").attr("value")
-    pizza_size_large = forms("form fieldset p").eq(2).find("label input").attr("value")
-    pizza_topping_bacon = forms("form fieldset").eq(1).find("p label input").attr("value")
-    pizza_topping_cheese = forms("form fieldset").eq(1).find("p").eq(1).find("label input").attr("value")
-    pizza_topping_onion = forms("form fieldset").eq(1).find("p").eq(2).find("label input").attr("value")
-    pizza_topping_mushroom = forms("form fieldset").eq(1).find("p").eq(3).find("label input").attr("value")
-    # delivery_time = forms("form p").eq(3).find("label input").attr("name")
-    # delivery_instruction = forms("form p").eq(4).find("label input").attr("name")
-    # submit = forms("forms p").eq(5).find("button").text()
-
-
-    return action, name, tel, email, pizza_size_small, pizza_size_medium, pizza_size_large , pizza_topping_bacon, pizza_topping_cheese,pizza_topping_onion, pizza_topping_mushroom #, delivery_time, delivery_instruction, submit
-
-
-# def filling_page():
-#     cust_name = pq()
-#     return cust_name
+    size = forms("form fieldset p label").find("input").attr("name")
+    topping = forms("form fieldset").eq(1).find("p label input").attr("name")
+    delivery_time = forms("form p").eq(10).find("label input").attr("name")
+    delivery_instruction = forms("form p").eq(11).find("label textarea").attr("name")
+    payload=f"{name}=kabir&{tel}=98079&{email}=kbr2gmia.com&{size}=large&{topping}=bacon&{topping}=cheese&{topping}=onion&{topping}=mushroom&{delivery_time}=11:00&{delivery_instruction}=hello fast food"
+    return payload , action 
 
 def main():
-    action, name, tel, email, pizza_size_small, pizza_size_medium, pizza_size_large , pizza_topping_bacon, pizza_topping_cheese,pizza_topping_onion , pizza_topping_mushroom  = next_page() #, delivery_time, delivery_instruction, submit
-    print(action)
-    print(name)
-    print(tel)
-    print(email)
-    print(pizza_size_small)
-    print(pizza_size_medium)
-    print(pizza_size_large)
-    print(pizza_topping_bacon)
-    print(pizza_topping_cheese)
-    print(pizza_topping_onion)
-    print(pizza_topping_mushroom)   
-    # print(delivery_time)
-    # print(delivery_instruction)
-    # print(submit)
+    payload, action = next_page() 
+    r = requests.post(base_url + action, data=payload)
+    print(r.json())
+    print(r.headers)
+   
+
+
+
+
 if __name__ == "__main__":
     main()
     
